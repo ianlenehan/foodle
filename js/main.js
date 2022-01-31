8b18e0407a9937c927677b6e894ff731d5b14d38
Ian Lenehan <ianlenehan@gmail.com>
2022-01-31 22:17:16 +0000

    fix clicks

--- js/main.js
+++ js/main.js
@@ -304,7 +304,7 @@ document.addEventListener("DOMContentLoaded", () => {
   function addKeyboardClicks() {
     const keys = document.querySelectorAll(".keyboard-row button");
     for (let i = 0; i < keys.length; i++) {
-      keys[i].onclick = ({ target }) => {
+      keys[i].addEventListener("click", ({ target }) => {
         const key = target.getAttribute("data-key");
 
         if (key === "enter") {
@@ -318,7 +318,7 @@ document.addEventListener("DOMContentLoaded", () => {
         }
 
         updateGuessedLetters(key);
-      };
+      });
     }
   }
 
@@ -332,21 +332,21 @@ document.addEventListener("DOMContentLoaded", () => {
     const span = document.getElementById("close-help");
 
     // When the user clicks on the button, open the modal
-    btn.onclick = function () {
+    btn.addEventListener("click", function () {
       modal.style.display = "block";
-    };
+    });
 
     // When the user clicks on <span> (x), close the modal
-    span.onclick = function () {
+    span.addEventListener("click", function () {
       modal.style.display = "none";
-    };
+    });
 
     // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function (event) {
+    window.addEventListener("click", function (event) {
       if (event.target == modal) {
         modal.style.display = "none";
       }
-    };
+    });
   }
 
   function updateStatsModal() {
@@ -372,21 +372,21 @@ document.addEventListener("DOMContentLoaded", () => {
     const span = document.getElementById("close-stats");
 
     // When the user clicks on the button, open the modal
-    btn.onclick = function () {
+    btn.addEventListener("click", function () {
       updateStatsModal();
       modal.style.display = "block";
-    };
+    });
 
     // When the user clicks on <span> (x), close the modal
-    span.onclick = function () {
+    span.addEventListener("click", function () {
       modal.style.display = "none";
-    };
+    });
 
     // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function (event) {
+    window.addEventListener("click", function (event) {
       if (event.target == modal) {
         modal.style.display = "none";
       }
-    };
+    });
   }
 });
